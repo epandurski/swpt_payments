3f722095eb25bb027c47de6b5d5dcf97b3331ce4
Evgeni Pandurski <epandurski@gmail.com>
2019-08-15 14:50:20 +0300

    Add CreatedOfferSignal model

--- swpt_payments/models.py
+++ swpt_payments/models.py
@@ -1,11 +1,43 @@
 import datetime
+import dramatiq
 from sqlalchemy.dialects import postgresql as pg
 from sqlalchemy.sql.expression import func
-from .extensions import db
+from .extensions import db, broker, MAIN_EXCHANGE_NAME
 
 
 def get_now_utc():
     return datetime.datetime.now(tz=datetime.timezone.utc)
+
+
+class Signal(db.Model):
+    __abstract__ = True
+
+    queue_name = None
+
+    @property
+    def event_name(self):  # pragma: no cover
+        model = type(self)
+        return f'on_{model.__tablename__}'
+
+    def send_signalbus_message(self):  # pragma: no cover
+        model = type(self)
+        if model.queue_name is None:
+            assert not hasattr(model, 'actor_name'), \
+                'SignalModel.actor_name is set, but SignalModel.queue_name is not'
+            actor_name = self.event_name
+            routing_key = f'events.{actor_name}'
+        else:
+            actor_name = model.actor_name
+            routing_key = model.queue_name
+        data = model.__marshmallow_schema__.dump(self)
+        message = dramatiq.Message(
+            queue_name=model.queue_name,
+            actor_name=actor_name,
+            args=(),
+            kwargs=data,
+            options={},
+        )
+        broker.publish_message(message, exchange=MAIN_EXCHANGE_NAME, routing_key=routing_key)
 
 
 class Offer(db.Model):
@@ -58,3 +90,11 @@ class Offer(db.Model):
 
 
 # TODO: Payment, PreparedTransfer
+
+
+class CreatedOfferSignal(Signal):
+    creditor_id = db.Column(db.BigInteger, primary_key=True)
+    offer_id = db.Column(pg.BYTEA(length=12), primary_key=True)
+    offer_request_id = db.Column(db.BigInteger, nullable=False)
+    created_at_ts = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+    issues = db.Column(pg.JSON, nullable=False, default={})
