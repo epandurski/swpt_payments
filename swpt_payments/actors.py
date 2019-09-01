fa99272502e77086efd5995d81db9ba5c52d5376
Evgeni Pandurski <epandurski@gmail.com>
2019-09-01 16:57:39 +0300

    Work in progress

--- swpt_payments/actors.py
+++ swpt_payments/actors.py
@@ -9,7 +9,7 @@ from . import procedures
 @broker.actor(queue_name=APP_QUEUE_NAME)
 def create_formal_offer(
         payee_creditor_id: int,
-        payee_offer_announcement_id: int,
+        offer_announcement_id: int,
         offer_secret: str,
         debtor_ids: List[int],
         debtor_amounts: List[int],
@@ -18,16 +18,58 @@ def create_formal_offer(
         reciprocal_payment_debtor_id: Optional[int] = None,
         reciprocal_payment_amount: int = 0) -> None:
 
-    """Creates a new offer."""
+    """Creates a new formal offer.
 
-    procedures.create_offer(
+    `payee_creditor_id` offers to deliver the goods or services
+    depicted in `description` if a payment is made to his account via
+    one of the debtors in `debtor_ids`, with the corresponding amount
+    in `debtor_amounts`. The offer will be valid until
+    `valid_until_ts`.
+
+    Before sending a message to this actor, the sender must create a
+    Formal Offer (FO) database record, with a primary key of
+    `(payee_creditor_id, offer_announcement_id)`, and status
+    "initiated". This record will be used to act properly on
+    `CreatedFromalOfferSignal` events.
+
+    TODO:
+
+    On received `CreatedFromalOfferSignal`, the status of the
+    corresponding FO record must be set to "created", and the received
+    value for `offer_id` -- recorded. The "prepared" FO record will
+    be, at some point, finalized (either by a
+    `SuccessfulPaymentSignal`, or by a `CanceledFormalOfferSignal`, or
+    by payment being made to the offer, or by using the
+    `cancel_formal_offer` actor), and the status set to
+    "finalized". The "finalized" CR record must not be deleted right
+    away, to avoid problems when the event handler ends up being
+    executed more than once.
+
+    If a `PreparedTransferSignal` is received, but a corresponding CR
+    record is not found, the newly prepared transfer must be
+    immediately dismissed (by sending a message to the
+    `finalize_prepared_transfer` actor with a zero `committed_amount`).
+
+    If a `PreparedTransferSignal` is received for an already
+    "prepared" or "finalized" CR record, the corresponding values of
+    `debtor_id`, `sender_creditor_id`, and `transfer_id` must be
+    compared. If they are the same, no action should be taken. If they
+    differ, the newly prepared transfer must be immediately dismissed.
+
+    If a `RejectedTransferSignal` is received, and the status of the
+    corresponding CR record is "initiated", the CR record must be
+    deleted. Otherwise, no action should be taken.
+
+    """
+
+    procedures.create_formal_offer(
         payee_creditor_id,
-        payee_offer_announcement_id,
+        offer_announcement_id,
         urlsafe_b64decode(offer_secret),
         debtor_ids,
         debtor_amounts,
-        description,
         iso8601.parse_date(valid_until_ts),
+        description,
         reciprocal_payment_debtor_id,
         reciprocal_payment_amount,
     )
@@ -40,7 +82,7 @@ def cancel_formal_offer(
 
     """Cancels an offer."""
 
-    procedures.create_offer(
+    procedures.cancel_formal_offer(
         payee_creditor_id,
         offer_id,
     )
@@ -54,7 +96,7 @@ def make_payment(
         payer_payment_order_seqnum: int,
         debtor_id: int,
         amount: int,
-        proof_secret: int,
+        proof_secret: str,
         payer_note: dict = {}) -> None:
 
     """Creates a payment order."""
--- swpt_payments/procedures.py
+++ swpt_payments/procedures.py
@@ -1,11 +1,53 @@
+from datetime import datetime, timezone
+from typing import Optional, List, TypeVar, Callable
 from .extensions import db
+from .models import FormalOffer, CreatedFormalOfferSignal, MIN_INT64, MAX_INT64
 
-# Define your database procedures here. For example:
-#
-#     @db.atomic
-#     def do_public_stuff(**kw):
-#         pass
-#
-#
-#     def _do_private_stuff(**kw):
-#         pass
+T = TypeVar('T')
+atomic: Callable[[T], T] = db.atomic
+
+
+@atomic
+def create_formal_offer(payee_creditor_id: int,
+                        offer_announcement_id: int,
+                        offer_secret: bytes,
+                        debtor_ids: List[int],
+                        debtor_amounts: List[int],
+                        valid_until_ts: Optional[datetime] = None,
+                        description: Optional[dict] = None,
+                        reciprocal_payment_debtor_id: Optional[int] = None,
+                        reciprocal_payment_amount: int = 0) -> None:
+    assert MIN_INT64 <= payee_creditor_id <= MAX_INT64
+    assert MIN_INT64 <= offer_announcement_id <= MAX_INT64
+    assert len(debtor_ids) == len(debtor_amounts)
+    assert all(MIN_INT64 <= debtor_id <= MAX_INT64 for debtor_id in debtor_ids)
+    assert all(0 <= debtor_amount <= MAX_INT64 for debtor_amount in debtor_amounts)
+    assert reciprocal_payment_debtor_id is None or MIN_INT64 <= reciprocal_payment_debtor_id <= MAX_INT64
+    assert 0 <= reciprocal_payment_amount <= MAX_INT64
+
+    formal_offer = FormalOffer(
+        payee_creditor_id=payee_creditor_id,
+        offer_secret=offer_secret,
+        debtor_ids=debtor_ids,
+        debtor_amounts=debtor_amounts,
+        valid_until_ts=valid_until_ts,
+        description=description,
+        reciprocal_payment_debtor_id=reciprocal_payment_debtor_id,
+        reciprocal_payment_amount=reciprocal_payment_amount,
+        created_at_ts=datetime.now(tz=timezone.utc),
+    )
+    db.session.add(formal_offer)
+    db.session.flush()
+    db.session.add(CreatedFormalOfferSignal(
+        payee_creditor_id=payee_creditor_id,
+        offer_id=formal_offer.offer_id,
+        created_at_ts=formal_offer.offer_id,
+        offer_announcement_id=offer_announcement_id,
+    ))
+
+
+@atomic
+def cancel_formal_offer(payee_creditor_id: int, offer_id: int) -> None:
+    formal_offer = FormalOffer.get_instance((payee_creditor_id, offer_id))
+    if formal_offer:
+        pass
